53dab865f4c204a43d67bb26d19ad6c32c376dd1
Joao Ferreira <joaomateusferr@gmail.com>
2022-02-03 19:11:20 -0300

    add configure odbc

---
+++ scripts/clickhouse/configure_odbc_driver.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#TESTS
+
+#check odbcinst paths
+#odbcinst -j
+
+if [ $(id -u) -ne 0 ]; then #this screipt equire root privileges (root id is 0)
+    echo 'No root privileges detected!'
+    echo 'Please, run this script as root'
+else
+
+    CLEAR_INI_FILES=1
+    ODBC_INI_PATH='/etc/odbc.ini'
+    ODBCINST_INI_PATH='/etc/odbcinst.ini'
+
+
+    if [ $CLEAR_INI_FILES -eq 1 ]; then
+        echo "" > $ODBC_INI_PATH
+        echo "" > $ODBCINST_INI_PATH
+    else
+        echo "" >> $ODBC_INI_PATH
+        echo "" >> $ODBCINST_INI_PATH
+    fi
+
+    echo "[ODBC Data Sources]" >> $ODBC_INI_PATH
+    echo "ClickHouse DSN (ANSI)    = ClickHouse ODBC Driver (ANSI)" >> $ODBC_INI_PATH
+    echo "ClickHouse DSN (Unicode) = ClickHouse ODBC Driver (Unicode)" >> $ODBC_INI_PATH
+    echo "" >> $ODBC_INI_PATH
+    echo "[ClickHouse DSN (ANSI)]" >> $ODBC_INI_PATH
+    echo "Driver      = ClickHouse ODBC Driver (ANSI)" >> $ODBC_INI_PATH
+    echo "Description = DSN (localhost) for ClickHouse ODBC Driver (ANSI)" >> $ODBC_INI_PATH
+    echo "" >> $ODBC_INI_PATH
+    echo "[ClickHouse DSN (Unicode)]" >> $ODBC_INI_PATH
+    echo "Driver      = ClickHouse ODBC Driver (Unicode)" >> $ODBC_INI_PATH
+    echo "Description = DSN (localhost) for ClickHouse ODBC Driver (Unicode)" >> $ODBC_INI_PATH
+    
+    echo "[ODBC Drivers]" >> $ODBCINST_INI_PATH
+    echo "ClickHouse ODBC Driver (ANSI)    = Installed" >> $ODBCINST_INI_PATH
+    echo "ClickHouse ODBC Driver (Unicode) = Installed" >> $ODBCINST_INI_PATH
+    echo "" >> $ODBCINST_INI_PATH
+    echo "[ClickHouse ODBC Driver (ANSI)]" >> $ODBCINST_INI_PATH
+    echo "Description = ODBC Driver (ANSI) for ClickHouse" >> $ODBCINST_INI_PATH
+    echo "Driver      = /usr/local/lib/libclickhouseodbc.so" >> $ODBCINST_INI_PATH
+    echo "Setup       = /usr/local/lib/libclickhouseodbc.so" >> $ODBCINST_INI_PATH
+    echo "UsageCount  = 1" >> $ODBCINST_INI_PATH
+    echo "" >> $ODBCINST_INI_PATH
+    echo "[ClickHouse ODBC Driver (Unicode)]" >> $ODBCINST_INI_PATH
+    echo "Description = ODBC Driver (Unicode) for ClickHouse" >> $ODBCINST_INI_PATH
+    echo "Driver      = /usr/local/lib/libclickhouseodbcw.so" >> $ODBCINST_INI_PATH
+    echo "Setup       = /usr/local/lib/libclickhouseodbcw.so" >> $ODBCINST_INI_PATH
+    echo "UsageCount  = 1" >> $ODBCINST_INI_PATH  
+fi
+
+#TESTS
+
+#check if the odbc driver is working
+#isql -v "ClickHouse DSN (Unicode)"
