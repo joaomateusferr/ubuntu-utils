16355a1775a54c240d58da7267afbd7bc90d5914
Joao Ferreira <joaomateusferr@gmail.com>
2022-02-26 12:07:57 -0300

    respond to a dns query

--- scripts/dns/recive_dns_query.py
+++ scripts/dns/recive_dns_query.py
@@ -1,6 +1,7 @@
 import socket
 import dns.resolver as dns
 from scapy.all import DNS
+from dnslib import DNSRecord
 
 port = 2525
 ip = '127.0.0.1'
@@ -12,10 +13,17 @@ while 1:
 
     data, addr = sock.recvfrom(512)
 
-    print("send")
+    forward_addr = ("8.8.8.8", 53) # dns and port
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    qname = "google.com" # query 
+    q = DNSRecord.question(qname)
+    client.sendto(bytes(q.pack()), forward_addr)
+    data, _ = client.recvfrom(1024)
+    d = DNSRecord.parse(data)
+    #print("r", str(d.rr[0].rdata)) # prints the A record of duckgo.com
 
-    decoded = DNS(data)
+    sock.sendto(data, addr)
 
-    print(decoded)
+    print(addr)
 
     #sock_gateway.send(data)
