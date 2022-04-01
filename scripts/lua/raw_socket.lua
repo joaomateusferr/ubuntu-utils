08167989ce83e806c76d08354ca045916a664c90
Joao Ferreira <joaomateusferr@gmail.com>
2022-03-31 22:28:33 -0300

    fix raw socket

--- scripts/lua/raw_socket.lua
+++ scripts/lua/raw_socket.lua
@@ -2,19 +2,19 @@ local socket = require "socket"
 local client = socket.connect('127.0.0.1',80)
 local Path = '/'
 local Host = 'test'
-local Body = 'test=1'
+local Body = 'Funcionou!!!!!'
 
 local Request =  'POST '..Path..' HTTP/1.1\r\n'
                ..'Host: '..Host..'\r\n'
-               ..'Connection: Keep-Alive\r\n'
+               --..'Connection: Keep-Alive\r\n'
                ..'Content-Type: text/plain\r\n'
                ..'Content-Length: '..#Body..'\r\n'
                ..'\r\n'
                .. Body
 
-print(client)
 if client then
     client:send(Request)
-    local s, status, partial = client:receive(1024)
-    print(partial)
+    s, status, partial = client:receive('*a')
+    print(s)
+    client:close()
 end
