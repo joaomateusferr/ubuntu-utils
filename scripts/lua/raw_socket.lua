77ba6cc7ec177a5d408fa2ba18bea9510c1b2126
Joao Ferreira <joaomateusferr@gmail.com>
2022-04-03 18:14:42 -0300

    update raw socket

--- scripts/lua/raw_socket.lua
+++ scripts/lua/raw_socket.lua
@@ -1,32 +1,76 @@
-local socket = require "socket"
-local host, port = '127.0.0.1', 80
-local tcp = assert(socket.tcp())
+local Socket = require 'socket'
+
+local Host, Port = '127.0.0.1', 80
+local Tcp = assert(Socket.tcp())
+
 local Path = '/'
 local Host = 'localhost'
 local Body = 'Funcionou!!!!!'
 
 local Request =  'POST '..Path..' HTTP/1.1\r\n'
                ..'Host: '..Host..'\r\n'
-               --..'Connection: Keep-Alive\r\n'
+               ..'User-Agent: Joao-POST\r\n'
                ..'Content-Type: text/plain\r\n'
                ..'Content-Length: '..#Body..'\r\n'
                ..'\r\n'
                .. Body
 
-for i=1,3 do
+Tcp:connect(Host, Port);
+Tcp:setoption('tcp-nodelay',true)
+Tcp:setoption('reuseaddr',true)
+Tcp:send(Request)
 
-  tcp:connect(host, port);
-  tcp:send(Request);
-               
-  while true do
-      local s, status, partial = tcp:receive()
-      local l = s or partial
-      print(l)
-      if status == "closed" then -- use KeepAlive Off
-        break
-      end
+local RequestStatus = ''
+local RequestHeader = ''
+local RequestBody = ''
+local RequestBodyStart = 0
+
+local I = 1
+
+while true do
+
+  local SocketS, SocketStatus, SocketPartial = Tcp:receive()
+  local Line = SocketS or SocketPartial
+
+  if I == 1 then
+
+    local StatusLine = Line
+    local Tokens = {}
+
+    for Value in StatusLine:gmatch("([^ ]+)") do
+      table.insert(Tokens, Value) 
+    end
+
+    if table.getn(Tokens) > 2 then
+      RequestStatus  =  Tokens[2]
+    end
+
   end
 
-  tcp:close()
+  if string.len(Line) == 0 then
+    RequestBodyStart = I
+  end
 
+  if (RequestBodyStart ~= 0 and I > RequestBodyStart) or (RequestBodyStart ~= 0 and I > RequestBodyStart and string.len(Line) ~= 0) then
+
+    if string.len(RequestBody) == 0 then
+      RequestBody = Line
+    else
+      RequestBody = RequestBody .. '\r\n' .. Line
+    end
+    
+  end
+      
+  if SocketStatus == "closed" then -- use KeepAlive Off
+    break
+  end
+
+  I = I + 1
+
+end
+
+Tcp:close()
+
+if RequestStatus == '200' then
+  print(RequestBody)
 end
