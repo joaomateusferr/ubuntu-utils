0d04607076cc0ddd1262a7c8c9f177f69e370d40
Joao Ferreira <joaomateusferr@gmail.com>
2022-04-16 17:27:32 -0300

    add ssh script

---
+++ scripts/install_and_enable_ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then #this screipt equire root privileges (root id is 0)
+    echo 'No root privileges detected!'
+    echo 'Please, run this script as root'
+else
+    apt update
+    apt-get install openssh-server
+    systemctl enable ssh
+    systemctl start ssh
+fi
+
+#run "sudo systemctl status ssh" to check if ssh is active
