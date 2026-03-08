96d256cb9e9d9963a6434027c5a4713e987891c8
Joao Ferreira <joaomateusferr@gmail.com>
2026-03-07 21:19:08 -0300

    Add cron schedules and manager

--- scripts/php/cron-manager.php
+++ scripts/php/cron-manager.php
@@ -6,9 +6,8 @@ class CronJob {
     private string $Minute;
     private string $Hour;
     private string $Host;
-    private string $Product;
 
-    public function __construct(array $Scripts, ?int $Minute = null, ?int $Hour = null, ?string $Host = null, ?string $Product = null){
+    public function __construct(array $Scripts, ?int $Minute = null, ?int $Hour = null, ?string $Host = null){
 
         if(empty($Scripts))
             throw new Exception("Script array cannot be empty in a cron job!");
@@ -17,12 +16,7 @@ class CronJob {
         $this->Minute = is_null($Minute) ? '*' :  str_pad($Minute, 2, '0', STR_PAD_LEFT);
         $this->Hour = is_null($Hour) ? '*' :  str_pad($Hour, 2, '0', STR_PAD_LEFT);
         $this->Host = is_null($Host) ? '*' : $Host;
-        $this->Product = is_null($Product) ? '*' : $Product;
 
-    }
-
-    public function getProduct() : string {
-        return $this->Product;
     }
 
     public function getHost() : string {
@@ -41,4 +35,97 @@ class CronJob {
         return $this->Scripts;
     }
 
+    public function getContentArray() : array {
+        return [
+            'Host' => $this->getHost(),
+            'Hour' => $this->getHour(),
+            'Minute' => $this->getMinute(),
+            'Scripts' => $this->getScripts(),
+        ];
+    }
+
 }
+
+class CronSchedules {
+
+    private array $Schedules;
+
+    public function __construct(){
+
+        $this->Schedules = [];
+
+    }
+
+    public function addJob(CronJob $Job){
+
+        $this->Schedules[] = $Job;
+
+    }
+
+    public function getContentArray() : array {
+
+        $Result = [];
+
+        foreach($this->Schedules as $Job){
+
+            $Result[] = $Job->getContentArray();
+
+        }
+
+        return $Result;
+
+    }
+
+}
+
+class CronManager {
+
+    private CronSchedules $Schedules;
+    private string $Hour;
+    private string $Minute;
+    private ?string $Host;
+
+    public function __construct(CronSchedules $Schedules, $Hour, int $Minute, ?string $Host = null) {
+
+        $this->Schedules = $Schedules;
+        $this->Hour = str_pad($Hour, 2, '0', STR_PAD_LEFT);
+        $this->Minute = str_pad($Minute, 2, '0', STR_PAD_LEFT);
+        $this->Host = is_null($Host) ? '*' : $Host;
+
+    }
+
+    private function isWildcard(string $Value): bool{
+        return $Value === '*';
+    }
+
+    public function getScriptsToRun(): array {
+
+        $Scripts = [];
+
+        foreach($this->Schedules as $Job){
+
+            var_dump($Job);exit;
+
+            $HostMatches = $this->isWildcard($Job->getHost()) || $this->isWildcard($this->Host) || $Job->getHost() === $this->Host;
+            $HourMatches   = $this->isWildcard($Job->getHour()) || $Job->getHour() === $this->Hour;
+            $MinuteMatches = $this->isWildcard($Job->getMinute()) || $Job->getMinute() === $this->Minute;
+
+            if ($HostMatches && $HourMatches && $MinuteMatches)
+                $Scripts = array_merge($Scripts, $Job->getScripts());
+
+
+        }
+
+        return $Scripts;
+
+    }
+
+}
+
+$Schedules = new CronSchedules();
+$Schedules->addJob(new CronJob(['test-1.php'], null, null, 'test-1'));
+
+$CronManager = new CronManager($Schedules, 0, 0);
+var_dump($CronManager->getScriptsToRun());
+
+
